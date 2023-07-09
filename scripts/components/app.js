1e00c6b9fa99a8617fcfb68acbbcaa3b7a15390d
nadeemb53 <nadeemb53@gmail.com>
2023-07-09 14:00:44 +0530

    sdk fixes

--- scripts/components/app.js
+++ scripts/components/app.js
@@ -1,32 +1,24 @@
-import m from 'mithril';
-import Session from '../models/session.js';
-import GameComponent from './game.js';
-import UpdateNotificationComponent from './update-notification.js';
+/* eslint-disable comma-dangle */
+import m from "mithril";
+import Session from "../models/session.js";
+import GameComponent from "./game.js";
+import UpdateNotificationComponent from "./update-notification.js";
 
 class AppComponent {
-
   oninit({ attrs = { roomCode: null } }) {
     this.session = new Session({
       url: window.location.origin,
-      roomCode: attrs.roomCode
+      roomCode: attrs.roomCode,
     });
   }
 
   view({ attrs = { roomCode: null } }) {
-    return m('div#app', [
+    return m("div#app", [
       // The UpdateNotificationComponent manages its own visibility
       m(UpdateNotificationComponent),
-      m('span#personal-site-link.nav-link.nav-link-left', [
-        m('a[href="https://github.com/caleb531/connect-four"]', 'View on GitHub')
-      ]),
-      m('span#github-link.nav-link.nav-link-right', [
-        'by ',
-        m('a[href="https://calebevans.me"]', 'Caleb Evans')
-      ]),
-      m(GameComponent, { session: this.session, roomCode: attrs.roomCode })
+      m(GameComponent, { session: this.session, roomCode: attrs.roomCode }),
     ]);
   }
-
 }
 
 export default AppComponent;
